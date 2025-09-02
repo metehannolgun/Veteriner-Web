77cc6f795a551bf47cceed8476716099e47461af
metehannolgun <metehannolgun@gmail.com>
2025-09-02 16:42:42 +0300

    Hero component updated

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import HeroTextContainer from './HeroTextContainer'
+import HeroImageContainer from './HeroImageContainer'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className='relative bg-gradient-to-br from-orange-50 to-teal-50 py-20'>
+      {/*Padding ve max width işlemleri */}
+      <div className='max-width-7xl mx-auto px-4 sm:px-6 lg:px-8 '>
+        {/* Grid sistemi, grid-cols-1:mobilde yukarıdan aşağıya lg:grid-cols-2= desktopta soldan sağa diz */}
+        <div className='grid grid-cols-1 lg:grid-cols-2 gap-12 items-center'>
+          {/* Sol taraf metin alanı */}
+           <HeroTextContainer />
+          {/* Sağ taraf görsel alanı */}
+          <HeroImageContainer />
+        </div>
+      </div>
+    </section>
   )
 }
 
---
+++ src/components/HeroTextContainer.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const HeroTextContainer = () => {
+  return (
+    <div>
+        <h1 className='text-4xl md:text-5xl font-bold mb-6'>
+        Kuşlarınızın Sağlığı
+         <span className='block text-orange-500'>Bizim Önceliğimiz</span>
+        </h1>
+        <p className='text-gray-600 mb-8 text-xl leading-relaxed'>
+        Profesyonel veterinerlik hizmetlerimizle, kuşlarınızın sağlığını ve mutluluğunu ön planda tutuyoruz. 
+        Sevimli dostlarınızın sağlık ihtiyaçları için profesyonel ve güvenilir hizmet.
+        </p>
+        {/* Butonlar için esnek konteyner , normal hali flox-col , ekran büyüdükçe row*/}
+        <div className='flex flex-col sm:flex-row gap-4'>
+            <a
+            className='bg-orange-600 text-white px-8 py-3 rounded-lg font-semibold hover:bg-orange-700 transition-colors text-center ' 
+            href='/iletişim'>Randevu Al
+            </a>
+            <a
+             className='bg-white text-orange-600 border-2 border-orange-600  px-8 py-3 rounded-lg font-semibold hover:bg-orange-50 transition-colors text-center ' 
+             href='tel:+905xxxxxxxxx'>Bizi Ara
+             </a>
+        </div>
+    </div>
+  )
+}
+
+export default HeroTextContainer
